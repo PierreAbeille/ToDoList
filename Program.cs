0bc36bf93dd0df62509bf5043dc18cecb7d8ed3b
Pierre <pierre.abeille@decasoft.fr>
2023-11-14 17:23:52 +0100

    Init Todolist app with SyncFusion components

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ToDoList;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<TodoService>();
+builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
